4b417d1d4af703dda32866084e02d6976d4c7cf8
ivy <unjust@gmail.com>
2019-06-22 16:55:59 -0500

    restarts working, restart container if found

--- server/startup.sh
+++ server/startup.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
+cd /
+rm -r radio_config
 echo "--------------- getting config bucket"
 gsutil cp -r gs://radiomolecula-nginx-config .
 mv radiomolecula-nginx-config radio_config
+
 echo "--------------- docker install"
 sudo apt-get install -y docker.io
-echo "--------------- docker build radiomolecula-image"
-sudo docker build -t radiomolecula-image radio_config
-echo "--------------- docker build radiomolecula-server"
-sudo docker run -d -p 1935:1935 --name radiomolecula-server radiomolecula-image
-echo "--------------- docker run radiomolecula-server complete"
 
+server=$(sudo docker ps -a -q -f "name=radiomolecula-server")
+if [ -n "$server" ]
+then
+  echo "--------------- restarting radiomolecula-server"
+  sudo docker start $server
+else
+  echo "--------------- docker build radiomolecula-image"
+  sudo docker build -t radiomolecula-image radio_config
+  echo "--------------- docker build radiomolecula-server"
+  sudo docker run -d -p 1935:1935 --name radiomolecula-server radiomolecula-image
+fi
+container_id=$(sudo docker ps -a -q -f "name=radiomolecula-server" -f "status=running")
+echo "--------------- radiomolecula-server should be up ${container_id}"
